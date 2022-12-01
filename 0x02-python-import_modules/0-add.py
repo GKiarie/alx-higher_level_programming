9906edaf76dfcdea7cad1eff4da8295d1b618bf8
GKiarie <igitaukiarie@gmail.com>
2022-12-01 09:06:11 -0800

    task 0 fix1

--- 0x02-python-import_modules/0-add.py
+++ 0x02-python-import_modules/0-add.py
@@ -1,6 +1,8 @@
 #!/usr/bin/python3
-import add_0 as mod
-a = 1
-b = 2
-sum = mod.add(a, b)
-print("{} + {} = {}".format(a, b, sum))
+if __name__ == "__main__":
+    import add_0 as mod
+    a = 1
+    b = 2
+    sum = mod.add(a, b)
+    print("{:d} + {:d} = {:d}".format(a, b, sum))
+
