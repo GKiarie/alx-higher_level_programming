7432357b126e7a9f1e70089b11a320741b0e2be7
GKiarie <igitaukiarie@gmail.com>
2022-12-20 11:16:19 -0800

    task 1

---
+++ 0x06-python-classes/1-square.py
@@ -0,0 +1,10 @@
+#!/usr/bin/python3
+"""Define class Square"""
+
+
+class Square:
+    """Reps a square"""
+
+    def __init__(self, size):
+        """ New square initialization"""
+        self.__size = size
