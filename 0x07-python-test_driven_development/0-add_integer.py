df4f4bec01aa5ba40008aef271c9d1bcde07e4c0
GKiarie <igitaukiarie@gmail.com>
2023-01-01 05:37:04 -0800

    task 0

---
+++ 0x07-python-test_driven_development/0-add_integer.py
@@ -0,0 +1,18 @@
+#!/usr/bin/python3
+
+"""Function to add integers"""
+
+
+def add_integer(a, b=98):
+    """
+    add two integers
+    """
+    if type(a) not in [int, float]:
+        raise TypeError('a must be an integer')
+    if type(b) not in [int, float]:
+        raise TypeError('b must be an integer')
+    if type(a) == float or type(b) == float:
+        a = int(a)
+        b = int(b)
+    return (a + b)
+    
