8a3c58d1cf6d59378e2bd5e91ba76455536870d8
GKiarie <igitaukiarie@gmail.com>
2023-01-09 10:51:09 -0800

    task 7

---
+++ 0x0A-python-inheritance/7-base_geometry.py
@@ -0,0 +1,17 @@
+#!/usr/bin/python3
+"""BaseGeometry Class"""
+
+
+class BaseGeometry:
+    """Represents base geometry"""
+
+    def area(self):
+        """area method to raise exception"""
+        raise Exception("area() is not implemented")
+
+    def integer_validator(self, name, value):
+        """function to validate values"""
+        if type(value) != int:
+            raise TypeError("{} must be an integer".format(name))
+        if value <= 0:
+            raise ValueError("{} must be greater than 0".format(value))
---
+++ 0x0A-python-inheritance/7-base_geometry.py~
@@ -0,0 +1,10 @@
+#!/usr/bin/python3
+"""BaseGeometry Class"""
+
+
+class BaseGeometry:
+    """Represents base geometry"""
+
+    def area(self):
+        """area method to raise exception"""
+        raise Exception("area() is not implemented")
