4fcff6e383d65d77e02a0d7a59aea2ad6ce7a890
GKiarie <igitaukiarie@gmail.com>
2023-01-09 09:23:37 -0800

    task 1

---
+++ 0x0A-python-inheritance/1-my_list.py
@@ -0,0 +1,16 @@
+#!/usr/bin/python3
+"""class Mylist inheriting from list"""
+
+
+class MyList(list):
+    """My list class"""
+
+    def __init__(self):
+        """list initiaization"""
+        super().__init__()
+
+    def print_sorted(self):
+        """
+        prints sorted list
+        """
+        print(sorted(self))
