d8f409accc1ca86593d4ed4784955969ef2ce983
GKiarie <igitaukiarie@gmail.com>
2023-01-11 09:57:06 -0800

    task 11

---
+++ 0x0B-python-input_output/11-student.py
@@ -0,0 +1,29 @@
+#!/usr/bin/python3
+"""A student Class"""
+
+
+class Student:
+    """Defines a student"""
+
+    def __init__(self, first_name, last_name, age):
+        self.first_name = first_name
+        self.last_name = last_name
+        self.age = age
+
+    def to_json(self, attrs=None):
+        """dictionary representation of instance"""
+        if attrs is None:
+            return self.__dict__
+        dict = {}
+        for attr in attrs:
+            try:
+                dict[attr] = self.__dict__[attr]
+            except Exception:
+                pass
+        return dict
+
+    def reload_from_json(self, json):
+        """load attributes from json"""
+        for k, v in json.items():
+            self.__dict__[k] = v
+    
