d3c6a22945997aa1f647e865bfaaa84434ad8f02
GKiarie <igitaukiarie@gmail.com>
2023-01-11 09:43:59 -0800

    task 10 fix

--- 0x0B-python-input_output/10-student.py
+++ 0x0B-python-input_output/10-student.py
@@ -12,7 +12,7 @@ class Student:
 
     def to_json(self, attrs=None):
         """dictionary representation of instance"""
-        if not attrs:
+        if attrs is None:
             return self.__dict__
         dict = {}
         for attr in attrs:
