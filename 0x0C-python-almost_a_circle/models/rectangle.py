6f0e4b5f9cba05452f017c062a219c07c57f2491
Gitau Kiarie <igitaukiarie@gmail.com>
2023-01-16 11:08:28 -0800

    task 2

---
+++ 0x0C-python-almost_a_circle/models/rectangle.py
@@ -0,0 +1,54 @@
+#!/usr/bin/python3
+"""Rectangle Class"""
+from models.base import Base
+
+
+class Rectangle(Base):
+    """Class Models a rectangle"""
+
+    def __init__(self, width, height, x=0, y=0, id=None):
+        self.__width = width
+        self.__height = height
+        self.__x = x
+        self.__y = y
+        super().__init__(id)
+
+    @property
+    def width(self):
+        """width getter"""
+        return self.__width
+
+    @width.setter
+    def width(self, value):
+        """width setter"""
+        self.__width = value
+
+    @property
+    def height(self):
+        """height getter"""
+        return self.__height
+
+    @height.setter
+    def height(self, value):
+        """height setter"""
+        self.__height = value
+
+    @property
+    def x(self):
+        """x getter"""
+        return self.__x
+
+    @x.setter
+    def x(self, value):
+        """height setter"""
+        self.__x = value
+
+    @property
+    def y(self):
+        """height getter"""
+        return self.__y
+
+    @y.setter
+    def y(self, value):
+        """height setter"""
+        self.__y = value
