f144f7fd0ee296114d49fe0b5eba1f8a0a61b924
Gitau Kiarie <igitaukiarie@gmail.com>
2023-03-13 11:29:01 +0000

    task 5

---
+++ 0x12-javascript-warm_up/5-to_integer.js
@@ -0,0 +1,15 @@
+#!/usr/bin/node
+
+const size = process.argv.length;
+let firstArg = process.argv[2];
+
+if (size >= 3) {
+  if (!isNaN(firstArg)) {
+    firstArg = parseInt(firstArg);
+    console.log(`my number: ${firstArg}`);
+  } else {
+    console.log('Not a number');
+  }
+} else {
+  console.log('Not a number');
+}
