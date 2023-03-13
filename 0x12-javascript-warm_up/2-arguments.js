2aff1ca2874b70a96bca7f7e9a79b238025c5830
Gitau Kiarie <igitaukiarie@gmail.com>
2023-03-13 06:20:15 +0000

    task 2

---
+++ 0x12-javascript-warm_up/2-arguments.js
@@ -0,0 +1,11 @@
+#!/usr/bin/node
+
+const size = process.argv.length;
+
+if (size === 2) {
+  console.log('No argument');
+} else if (size === 3) {
+  console.log('Argument found');
+} else if (size > 3) {
+  console.log('Arguments found');
+}
