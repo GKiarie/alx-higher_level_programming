4bb76473e3d8e2d44f235b1d92242665a246a4fd
Gitau Kiarie <igitaukiarie@gmail.com>
2023-03-13 11:55:35 +0000

    task 6 fix

--- 0x12-javascript-warm_up/6-multi_languages_loop.js
+++ 0x12-javascript-warm_up/6-multi_languages_loop.js
@@ -1,10 +1,10 @@
 #!/usr/bin/node
 
-const myArray = ["C is fun", "Python is cool", "JavaScript is amazing"];
+const myArray = ['C is fun', 'Python is cool', 'JavaScript is amazing'];
 
 let i = 0;
 
 while (i < myArray.length) {
-	console.log(myArray[i]);
-	i++;
+  console.log(myArray[i]);
+  i++;
 }
