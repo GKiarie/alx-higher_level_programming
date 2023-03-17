d1854f671718d13ea23a29ed0a13dad28e69dd96
Gitau Kiarie <igitaukiarie@gmail.com>
2023-03-17 04:25:27 +0000

    task 2

--- 0x0F-python-object_relational_mapping/2-my_filter_states.py
+++ 0x0F-python-object_relational_mapping/2-my_filter_states.py
@@ -1,11 +1,13 @@
 #!/usr/bin/env python3
-"""Script that lists all entries from
-a table in a db where name startswith 'N'"""
+"""Script that takes in an argument and displays
+all the values in states tb of db where name matches
+argument"""
 import MySQLdb
 from sys import argv
 
 if __name__ == "__main__":
     db = MySQLdb.connect(user=argv[1], passwd=argv[2], db=argv[3])
     cur = db.cursor()
-    cur.execute("SELECT * FROM states WHERE name LIKE 'N%'")
+    name = argv[4]
+    cur.execute("SELECT * FROM states WHERE name = '{}'".format(argv[4]))
     [print(state) for state in cur.fetchall()]
