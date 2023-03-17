2ef740970bc21d781374600e97686da2a6d542cd
Gitau Kiarie <igitaukiarie@gmail.com>
2023-03-17 03:56:06 +0000

    task 1

---
+++ 0x0F-python-object_relational_mapping/1-filter_states.py
@@ -0,0 +1,11 @@
+#!/usr/bin/env python3
+"""Script that lists all entries from
+a table is a database"""
+import MySQLdb
+from sys import argv
+
+if __name__ == "__main__":
+    db = MySQLdb.connect(user=argv[1], passwd=argv[2], db=argv[3])
+    cur = db.cursor()
+    cur.execute("SELECT * FROM states WHERE name LIKE 'N%'")
+    [print(state) for state in cur.fetchall()]
