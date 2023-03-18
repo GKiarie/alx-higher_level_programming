49a836525a6e9140ce2f2b7c95e2d8a2c70f1fd1
Gitau Kiarie <igitaukiarie@gmail.com>
2023-03-18 07:37:17 +0000

    task 2

--- 0x0F-python-object_relational_mapping/2-my_filter_states.py
+++ 0x0F-python-object_relational_mapping/2-my_filter_states.py
@@ -1,13 +1,28 @@
-#!/usr/bin/env python3
-"""Script that takes in an argument and displays
-all the values in states tb of db where name matches
-argument"""
+#!/usr/bin/python3
+""" selecting with mysqldb """
 import MySQLdb
-from sys import argv
+import sys
+
 
 if __name__ == "__main__":
-    db = MySQLdb.connect(user=argv[1], passwd=argv[2], db=argv[3])
-    cur = db.cursor()
-    name = argv[4]
-    cur.execute("SELECT * FROM states WHERE name = '{}'".format(argv[4]))
-    [print(state) for state in cur.fetchall()]
+    try:
+        connection = MySQLdb.connect(
+            host="localhost",
+            user=sys.argv[1],
+            passwd=sys.argv[2],
+            port=3306,
+            db=sys.argv[3]
+        )
+    except MySQLdb.Error:
+        print("error connecting")
+    cur = connection.cursor()
+    try:
+        cur.execute("SELECT * FROM states WHERE BINARY name = '{}'\
+ ORDER BY states.id".format(sys.argv[4]))
+        rows = cur.fetchall()
+        for row in rows:
+            print(row)
+    except MySQLdb.Error:
+        print("execution failed")
+    cur.close()
+    connection.close()
