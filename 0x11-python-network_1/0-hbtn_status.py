e3af543a43c38130c3ad9a75d18aa67a291cbd6a
Gitau Kiarie <igitaukiarie@gmail.com>
2023-03-31 17:39:01 +0000

    url fetching script pycode update

--- 0x11-python-network_1/0-hbtn_status.py
+++ 0x11-python-network_1/0-hbtn_status.py
@@ -1,10 +1,10 @@
 #!/usr/bin/python3
 """script that fetches https://alx-intranet.hbtn.io/status"""
-import urllib.request
+from urllib.request import urlopen
 
 
 if __name__ == "__main__":
-    with urllib.request.urlopen('https://alx-intranet.hbtn.io/status') as response:
+    with urlopen('https://alx-intranet.hbtn.io/status') as response:
         html = response.read()
         print("Body response:")
         print("\t- type: {}".format(type(html)))
