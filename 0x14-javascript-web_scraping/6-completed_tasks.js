2779633f69db72b6a372dcc19173921af5af9a8f
GKiarie <igitaukiarie@gmail.com>
2023-04-25 19:24:16 +0000

    task 6

---
+++ 0x14-javascript-web_scraping/6-completed_tasks.js
@@ -0,0 +1,18 @@
+#!/usr/bin/node
+// script that computes the number of tasks completed by user id.
+const request = require('request');
+
+request(process.argv[2], function (error, response, body) {
+  if (!error) {
+    const tasks = JSON.parse(body);
+    const completed = {};
+    tasks.forEach((task) => {
+      if (task.completed && completed[task.userId] === undefined) {
+        completed[task.userId] = 1;
+      } else if (task.completed) {
+        completed[task.userId] += 1;
+      }
+    });
+    console.log(completed);
+  }
+});
